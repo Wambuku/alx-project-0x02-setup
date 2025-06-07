274ede2f548e0874f0cee9117d037e6fbb49cdda
Juliet Wambuku <juliet@buupass.com>
2025-06-07 13:36:51 +0300

    Reusable Cards

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,11 @@
+import { CardProps} from "@/interfaces";
+
+export default function Card({ title, content }: CardProps) {
+    return (
+        <div className="bg-white shadow-md rounded-md p-4 border border-gray-200 hover:shadow-lg transition duration-300">
+            <h2 className="text-xl font-semibold mb-2 text-blue-700">{title}</h2>
+            <p className="text-gray-700">{content}</p>
+         </div>
+         
+    );
+}
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,12 +1,17 @@
 import Header from "@/components/layout/Header";
+import Card from "@/components/common/Card";
 
 export default function HomePage() {
-    return (
-     <>
-        <Header/>
-        <main className="min-h-screen flex items-center justify-center bg-gray-100">
-            <h1 className="text-4xl font-bold text-gray-800">Welcome to Alx Next.js Project</h1>
-        </main>
-     </>
- );
+  return (
+    <>
+      <Header />
+      <main className="p-6 space-y-4">
+        <h1 className="text-3xl font-bold mb-4">Welcome to the Home Page</h1>
+
+        <Card title="Card 1" content="This is the first reusable card component." />
+        <Card title="Card 2" content="Here is another card with different content." />
+        <Card title="Card 3" content="You can reuse this component as much as needed." />
+      </main>
+    </>
+  );
 }
