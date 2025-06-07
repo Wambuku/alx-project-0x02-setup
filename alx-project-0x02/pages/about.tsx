c19da9839218a7c279d7313ddadb9b4b74db4b23
Juliet Wambuku <juliet@buupass.com>
2025-06-07 13:48:39 +0300

    Reusable Button

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,12 +1,19 @@
 import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
 
 export default function AboutPage() {
-    return (
-        <>
-        <Header />
-        <main className="p-6">
-          <h1 className="text-3xl font-bold text-green-700">About This Project</h1>
-        </main>
-      </>
-    );
+  return (
+    <>
+      <Header />
+      <main className="p-6 space-y-6">
+        <h1 className="text-3xl font-bold mb-4">Reusable Buttons</h1>
+
+        <div className="space-y-4">
+          <Button label="Small Button" size="small" shape="rounded-sm" />
+          <Button label="Medium Button" size="medium" shape="rounded-md" />
+          <Button label="Large Button" size="large" shape="rounded-full" />
+        </div>
+      </main>
+    </>
+  );
 }
