a8eb067df16ddeb68499ad667ab0f85e9e89c2c1
Juliet Wambuku <juliet@buupass.com>
2025-06-07 13:52:49 +0300

    Navigatin and Layout

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -4,9 +4,9 @@ export default function Header() {
   return (
     <header className="bg-gray-800 text-white p-4">
       <nav className="flex gap-6">
-        <Link href="/" className="hover:underline">Home (Index)</Link>
         <Link href="/home" className="hover:underline">Home</Link>
         <Link href="/about" className="hover:underline">About</Link>
+        <Link href="/posts" className="hover:underline">Posts</Link>
       </nav>
     </header>
   );
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+
+export default function PostsPage() {
+  return (
+    <>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-3xl font-bold text-purple-700">Posts Page</h1>
+        <p>Posts will be displayed here later.</p>
+      </main>
+    </>
+  );
+}
